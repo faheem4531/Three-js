44894d6c75701bb15b908e8500ab2834575934cb
faheem4531 <faheem4531@gmail.com>
2023-10-24 19:48:26 +0500

    apply position/ scale/ axesHelper

--- lessons/04/starter/src/script.js
+++ lessons/04/starter/src/script.js
@@ -1,8 +1,12 @@
 import * as THREE from "three"
+import "./style.css"
+
 
 
 //  Scene 
 const scene = new THREE.Scene();
+
+
 
 
 // Red Cube 
@@ -11,16 +15,47 @@ const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+
+// position 
+// mesh.position.x = 0.7
+// mesh.position.y = - 0.6
+// mesh.position.z = 1
+mesh.position.set(0.7, -0.6, 1);
+
+
+// Scale 
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2, 0.5, 0.5)
+
+
+
+
+// Axes helper 
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+
+
+
 // Sizes
 const sizes = {
   width: 800,
   height: 600
 }
 
+
+
+
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+// console.log(mesh.position.distanceTo(camera.position));
+
+
+
 
 // Renderer
 const canvas = document.querySelector('.webgl')
