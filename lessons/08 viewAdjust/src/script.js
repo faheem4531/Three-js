e6102bc6234ad891dd195380e19ec0831e9d60c2
faheem4531 <faheem4531@gmail.com>
2023-10-25 20:23:09 +0500

    if window's width resize ...

--- lessons/08 viewAdjust/src/script.js
+++ lessons/08 viewAdjust/src/script.js
@@ -1,6 +1,5 @@
 import * as THREE from "three"
 import "./style.css"
-import gsap from "gsap"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
 // Curser
@@ -13,7 +12,6 @@ window.addEventListener('mousemove', (event) => {
   curser.x = event.clientX / sizes.width - 0.5
   curser.y = -(event.clientY / sizes.height - 0.5)
 })
-
 
 
 //  Scene 
@@ -32,26 +30,28 @@ const canvas = document.querySelector('.webgl')
 
 // Sizes
 const sizes = {
-  width: 800,
-  height: 600
+  width: window.innerWidth,
+  height: window.innerHeight
 }
+
+window.addEventListener('resize', () => {
+
+  // Update Size 
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update Camera 
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer 
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 4))
+})
 
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-
-// const aspectRatio = sizes.width / sizes.height
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspectRatio,
-//   1 * aspectRatio,
-//   1,
-//   -1,
-//   0.1,
-//   100
-// )
-
-// camera.position.x = 2
-// camera.position.y = 2
 camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
@@ -67,18 +67,10 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
 
 
 // // Clock 
 const clock = new THREE.Clock()
-
-
-
-// Animations 
-// gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
-// gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
-
 
 const tick = () => {
 
