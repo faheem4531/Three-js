785904c776d812c6e740ddef40e6071d2c18f8f4
faheem4531 <faheem4531@gmail.com>
2023-10-26 17:46:52 +0500

    add assets

--- lessons/11 textures (add images to model)/src/script.js
+++ lessons/11 textures (add images to model)/src/script.js
@@ -1,114 +1,84 @@
-import * as THREE from "three"
-import "./style.css"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-// Curser
-const curser = {
-  x: 0,
-  y: 0
-}
-window.addEventListener('mousemove', (event) => {
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
-  curser.x = event.clientX / sizes.width - 0.5
-  curser.y = -(event.clientY / sizes.height - 0.5)
-})
+// Scene
+const scene = new THREE.Scene()
 
-
-//  Scene 
-const scene = new THREE.Scene();
-
-
-// Red Cube 
+/**
+ * Object
+ */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-
-// Canvas 
-const canvas = document.querySelector('.webgl')
-
-// Sizes
+/**
+ * Sizes
+ */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
 window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  // Update Size 
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update Camera 
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
-
-  // Update renderer 
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 4))
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-
-window.addEventListener('dblclick', () => {
-
-  if (!document.fullscreenElement) {
-    canvas.requestFullscreen()
-  }
-  else {
-    document.exitFullscreen()
-  }
-})
-
-// Camera
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 2
-camera.lookAt(mesh.position)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
 scene.add(camera)
 
-
-// Controls 
+// Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-
-// // Clock 
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
 
+    // Update controls
+    controls.update()
 
-  // const ellapsedTime = clock.getElapsedTime()
+    // Render
+    renderer.render(scene, camera)
 
-  // camera.position.y = Math.sin(ellapsedTime)
-  // camera.position.x = Math.cos(ellapsedTime)
-
-  // mesh.rotation.y = ellapsedTime
-  // mesh.rotation.x += 0.01
-  // mesh.rotation.z += 0.01
-
-
-  // Update Camera 
-  // camera.position.x = Math.sin(curser.x * Math.PI * 2) * 3
-  // camera.position.z = Math.cos(curser.x * Math.PI * 2) * 3
-  // camera.position.y = Math.sin(curser.y * Math.PI * 2) * 3
-  // camera.position.z = Math.cos(curser.y * Math.PI * 2) * 3
-  // camera.lookAt(mesh.position)
-
-
-  // Update controls 
-  controls.update()
-
-  // Render 
-  renderer.render(scene, camera)
-  window.requestAnimationFrame(tick)
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
 
 tick()
