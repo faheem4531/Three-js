2dc2818c3a4cece4ec9c0d93fd2a7016126c5664
faheem4531 <faheem4531@gmail.com>
2023-10-28 18:56:51 +0500

    center the text geometry

--- lessons/13 TextGeometry/src/script.js
+++ lessons/13 TextGeometry/src/script.js
@@ -25,57 +25,59 @@ const textureLoader = new THREE.TextureLoader()
 //                           Fonts
 const fontLoader = new FontLoader()
 fontLoader.load(
-    '/fonts/helvetiker_regular.typeface.json ',
-    (font) => {
-        const textGeometry = new TextGeometry(
-            'faheem',
-            {
-                font: font,
-                size: 0.5,
-                height: 0.2,    //  depth
-                curveSegments: 12,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 5
-            }
-        )
-        const textMaterial = new THREE.MeshBasicMaterial()
-        const text = new THREE.Mesh(textGeometry, textMaterial)
-        scene.add(text)
-    }
-)
-/**
- * Object
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+  '/fonts/helvetiker_regular.typeface.json ',
+  (font) => {
+    const textGeometry = new TextGeometry(
+      'Mushab Faheem',
+      {
+        font: font,
+        size: 0.5,
+        height: 0.2,    //  depth
+        curveSegments: 5,
+        bevelEnabled: true,
+        bevelThickness: 0.03,
+        bevelSize: 0.02,
+        bevelOffset: 0,
+        bevelSegments: 4
+      }
+    )
+    // textGeometry.computeBoundingBox()  // to make text geomatery in center manually 
+    // textGeometry.translate(
+    //   -(textGeometry.boundingBox.max.x - 0.02) * 0.5,   // subtract x and y values from bevelSize to make it center of the its axes
+    //   - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+    //   - (textGeometry.boundingBox.max.z - 0.03) * 0.5   // subtract z value from bevelThickness to make it center of the its axes
+    // )
+
+    textGeometry.center()  // to make text geomatery in center with threejs 
+
+    const textMaterial = new THREE.MeshBasicMaterial()
+    const text = new THREE.Mesh(textGeometry, textMaterial)
+    scene.add(text)
+  }
 )
 
-scene.add(cube)
+
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
 window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -88,7 +90,9 @@ camera.position.y = 1
 camera.position.z = 2
 scene.add(camera)
 
-// Controls
+
+
+// Controls    // camera control system
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
@@ -96,7 +100,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -107,16 +111,16 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
