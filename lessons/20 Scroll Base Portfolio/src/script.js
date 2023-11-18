5c2fceca2fa4e5e73d2dc864e146b8361dd201c7
faheem4531 <faheem4531@gmail.com>
2023-11-19 01:33:42 +0500

    add ease effet ==> to make objects movement smoth

--- lessons/20 Scroll Base Portfolio/src/script.js
+++ lessons/20 Scroll Base Portfolio/src/script.js
@@ -150,17 +150,20 @@ window.addEventListener('mousemove', (event) => {
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
 
     //Animate camera
     camera.position.y = - scrollY / sizes.height * objectsDistance
 
     const parallaxX = cursor.x
     const parallaxY = - cursor.y
-    cameraGroup.position.x = parallaxX
-    cameraGroup.position.y = parallaxY
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 3 * deltaTime // just for easing camera effect
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 3 * deltaTime
 
 
     //Animate Meshes
