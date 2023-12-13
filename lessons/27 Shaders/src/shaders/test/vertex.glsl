67ba88732b685cd6286d92e2d89334e079d1b233
faheem4531 <faheem4531@gmail.com>
2023-12-14 03:48:49 +0500

    apply textures flag

--- lessons/27 Shaders/src/shaders/test/vertex.glsl
+++ lessons/27 Shaders/src/shaders/test/vertex.glsl
@@ -5,9 +5,9 @@
   uniform float uTime;
 
   attribute vec3 position;
-  attribute float aRandom;
+  attribute vec2 uv;
 
-  // varying float vRandom;
+  varying vec2 vUv;
 
 
   void main()
@@ -15,8 +15,6 @@
     vec4 modelPosition = modelMatrix * vec4( position, 1.0);
     modelPosition.z += sin( modelPosition.x * uFrequency.x - uTime ) * 0.1;
     modelPosition.z += sin( modelPosition.y * uFrequency.y - uTime ) * 0.1;
-    // modelPosition.z += aRandom * 0.1;
-
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
@@ -24,5 +22,6 @@
 
     gl_Position = projectedPosition;
 
-    // vRandom = aRandom;
+    vUv=uv;
+
   }
