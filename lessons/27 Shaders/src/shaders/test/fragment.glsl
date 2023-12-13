60986e4acef624a6c42bcde09cfcaa28ed959434
faheem4531 <faheem4531@gmail.com>
2023-12-14 03:05:18 +0500

    varing => vertex to fragment

--- lessons/27 Shaders/src/shaders/test/fragment.glsl
+++ lessons/27 Shaders/src/shaders/test/fragment.glsl
@@ -1,5 +1,8 @@
-    precision mediump float;
+precision mediump float;
 
-    void main(){
-        gl_FragColor = vec4 (1.0,0.0,0.0,1.0);
-    }
+varying float vRandom;
+
+
+void main(){
+  gl_FragColor = vec4 (vRandom, vRandom * 0.5, 0.0, 1.0);
+}
--- lessons/27 Shaders/src/shaders/test/vertex.glsl
+++ lessons/27 Shaders/src/shaders/test/vertex.glsl
@@ -1,10 +1,24 @@
-  uniform mat4 projectionMatrix;
-  uniform mat4 viewMatrix;
-  uniform mat4 modelMatrix;
+  uniform mat4 projectionMatrix;  //apply transformations relative to the Mest (Position,scale,rotation)
+  uniform mat4 viewMatrix;  // apply transformations relative to the camera (position,rotation, field of view, near,far)
+  uniform mat4 modelMatrix;  // transform the coordinates into the clip space cordinates
 
   attribute vec3 position;
+  attribute float aRandom;
+
+  varying float vRandom;
+
 
   void main()
   {
-    gl_Position = projectionMatrix * viewMatrix * vec4(position,1.0);
+    vec4 modelPosition = modelMatrix * vec4( position, 1.0);
+    // modelPosition.z += sin( modelPosition.x * 10.0 ) * 0.1;
+    modelPosition.z += aRandom * 0.1;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+
+    gl_Position = projectedPosition;
+
+    vRandom = aRandom;
   }
